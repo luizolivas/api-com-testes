2fc6533e9de5de6da997f4fe9a15473ddfcc8075
luizolivas <luizolivas@gmail.com>
2024-01-15 10:35:48 -0300

    Relacionamentos de classes com pedido

--- introApiWeb/Models/Pessoa.cs
+++ introApiWeb/Models/Pessoa.cs
@@ -1,11 +1,19 @@
-﻿namespace introApiWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace introApiWeb.Models
 {
     public class Pessoa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string? Nome { get; set; }
 
         public string? Tel {  get; set; }
+
+        public ICollection<Pedido> Pedido { get; set; }
+
     }
 }
--- introApiWeb/Models/Produto.cs
+++ introApiWeb/Models/Produto.cs
@@ -1,7 +1,12 @@
-﻿namespace introApiWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace introApiWeb.Models
 {
     public class Produto
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string? Nome { get; set; }
