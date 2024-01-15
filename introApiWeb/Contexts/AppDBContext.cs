676c72af13606118a60bd0bf20e47a585924d871
luizolivas <luizolivas@gmail.com>
2024-01-15 10:34:37 -0300

    cria classe pedido

--- introApiWeb/Contexts/AppDBContext.cs
+++ introApiWeb/Contexts/AppDBContext.cs
@@ -14,5 +14,22 @@ namespace introApiWeb.Contexts
         public DbSet<Pessoa> Pessoas { get; set; }
 
         public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Pessoa>()
+                .HasMany(p => p.Pedido)
+                .WithOne(pedido => pedido.Pessoa)
+                .HasForeignKey(pedido => pedido.PessoaId);
+                
+        }
+
+
+        public DbSet<introApiWeb.Models.Pedido>? Pedido { get; set; }
     }
 }
---
+++ introApiWeb/Models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿namespace introApiWeb.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int PessoaId { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+    }
+}
