4a43467fd6e5c9148ac8ac3178b838b0fbd4dcca
luizolivas <luizolivas@gmail.com>
2024-01-15 10:38:22 -0300

    Inclui service e controller pedido

---
+++ introApiWeb/Controllers/PedidoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using introApiWeb.Contexts;
+using introApiWeb.Models;
+using introApiWeb.Services;
+
+namespace introApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        private readonly PedidoService _PedidoService;
+
+        public PedidoController(PedidoService Pedidoervice)
+        {
+            _PedidoService = Pedidoervice;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pedido>>> GetAllPedido()
+        {
+            List<Pedido> Pedido = await _PedidoService.getAllPedido();
+            return Ok(Pedido);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddPedido(Pedido Pedido)
+        {
+            try
+            {
+                await _PedidoService.AddPedido(Pedido);
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Falha ao adicionar Pedido: {ex.Message}");
+
+            }
+
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePedido(int id)
+        {
+            try
+            {
+                await _PedidoService.DeletePedido(id);
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Falha ao remover Pedido: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ introApiWeb/Services/PedidoService.cs
@@ -0,0 +1,42 @@
+﻿using introApiWeb.Contexts;
+using introApiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace introApiWeb.Services
+{
+    public class PedidoService
+    {
+        private readonly AppDBContext _context;
+
+        public PedidoService(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Pedido>> getAllPedido()
+        {
+            return await _context.Pedidos.ToListAsync();
+        }
+
+        public async Task AddPedido(Pedido Pedido)
+        {
+            _context.Pedidos.Add(Pedido);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePedido(int PedidoId)
+        {
+            var p = _context.Pedidos.Find(PedidoId);
+
+            if (p is null)
+            {
+                return;
+            }
+
+            _context.Pedidos.Remove(p);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
